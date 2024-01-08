c1332461e3ebb4aa38123435459c13fb8bc60575
Jakub Sencio <jakub.sencio@microsoft.wsei.edu.pl>
2024-01-08 11:20:51 +0100

    1

---
+++ Labolatorium 2/Controllers/CalculatorController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Labolatorium_2.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Result(double a, double b)
+        {
+            string op = Request.Query["op"];
+            double c = a + b;
+            op = c.ToString();
+            ViewBag.Op = op;
+            return View();
+        }
+        public enum Operator
+        {
+            Unknown, Add, Mul, Sub, Div
+        }
+    }
+}
--- Labolatorium 2/Controllers/HomeController.cs
+++ Labolatorium 2/Controllers/HomeController.cs
@@ -1,8 +1,8 @@
-﻿using Labolatorium_1.Models;
+﻿using Labolatorium_2.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
-namespace Labolatorium_1.Controllers
+namespace Labolatorium_2.Controllers
 {
     public class HomeController : Controller
     {
@@ -27,23 +27,6 @@ namespace Labolatorium_1.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-        public IActionResult About(Operator op)
-        {
-            ViewBag.Op = op;
-            return View();
-        }
-        public IActionResult Calculator(double a, double b)
-        {
-            string op = Request.Query["op"];
-            double c = a + b;
-            op = c.ToString();
-            ViewBag.Op = op;
-            return View();         
-        }
-        public enum Operator
-        {
-            Unknown, Add, Mul, Sub, Div
         }
     }
 }
