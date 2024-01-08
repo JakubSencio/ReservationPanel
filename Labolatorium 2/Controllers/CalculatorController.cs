60192da95da358e8a8a6c9fd5cdbdf1ec7b83d04
Jakub Sencio <jakub.sencio@microsoft.wsei.edu.pl>
2024-01-08 11:40:28 +0100

    .

--- Labolatorium 2/Controllers/CalculatorController.cs
+++ Labolatorium 2/Controllers/CalculatorController.cs
@@ -8,10 +8,10 @@ namespace Labolatorium_2.Controllers
         {
             return View();
         }
-        public IActionResult Result(double a, double b)
+        public IActionResult Result(double x, double y)
         {
             string op = Request.Query["op"];
-            double c = a + b;
+            double c = x + y;
             op = c.ToString();
             ViewBag.Op = op;
             return View();
@@ -19,6 +19,10 @@ namespace Labolatorium_2.Controllers
         public enum Operator
         {
             Unknown, Add, Mul, Sub, Div
+        }
+        public IActionResult Form()
+        {
+            return View();
         }
     }
 }
---
+++ Labolatorium 2/Models/Calculator.cs
@@ -0,0 +1,55 @@
+﻿namespace Labolatorium_2.Models
+{
+    public class Calculator
+    {
+        public Operators? Operator { get; set; }
+        public double? X { get; set; }
+        public double? Y { get; set; }
+
+        public enum Operators
+        {
+            Unknown, Add, Mul, Sub, Div
+        }
+        public String Op
+        {
+            get
+            {
+                switch (Operator)
+                {   
+                    case Operators.Add:
+                        return "+";
+                    case Operators.Div: 
+                        return "-";
+                    case Operators.Mul:
+                        return "*";
+                    case Operators.Sub:
+                        return "/";
+                default:
+                        return "";
+                }
+            }
+        }
+
+        public bool IsValid()
+        {
+            return Operator != null && X != null && Y != null;
+        }
+
+        public double Calculate()
+        {
+            switch (Operator)
+            {
+                case Operators.Add:
+                    return (double)(X + Y);
+                case Operators.Div:
+                    return (double)(X - Y);
+                case Operators.Mul:
+                    return (double)(X * Y);
+                case Operators.Sub:
+                    return (double)(X / Y);
+                  
+            default: return double.NaN;
+            }
+        }
+    }
+}
