9ca7d40be9457808e99e74cac973c9342cbb23bb
Jakub Sencio <jakub.sencio@microsoft.wsei.edu.pl>
2024-01-10 14:37:21 +0100

    Lab 2 DONE

--- Labolatorium 2/Controllers/BirthController.cs
+++ Labolatorium 2/Controllers/BirthController.cs
@@ -1,12 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Labolatorium_2.Models;
 
 namespace Labolatorium_2.Controllers
 {
     public class BirthController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            return View(new BirthFormModel());
+        }
+
+        [HttpPost]
+        public IActionResult Index(BirthFormModel model)
+        {
+            if (model.IsValid())
+            {
+                int age = model.CalculateAge();
+                ViewBag.Message = $"Cześć {model.Name}, masz {age} lat(a).";
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Wprowadź poprawne dane.";
+            }
+
+            return View(model);
         }
     }
 }
---
+++ Labolatorium 2/Models/BirthFormModel.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Labolatorium_2.Models
+{
+    public class BirthFormModel
+    {
+        [Required(ErrorMessage = "Pole 'Imię' jest wymagane.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Pole 'Data urodzenia' jest wymagane.")]
+        [DataType(DataType.Date)]
+        public DateTime BirthDate { get; set; }
+
+        public bool IsValid()
+        {
+            return !string.IsNullOrEmpty(Name) && BirthDate < DateTime.Now;
+        }
+
+        public int CalculateAge()
+        {
+            DateTime currentDate = DateTime.Now;
+            int age = currentDate.Year - BirthDate.Year;
+
+            if (currentDate.Month < BirthDate.Month || (currentDate.Month == BirthDate.Month && currentDate.Day < BirthDate.Day))
+            {
+                age--;
+            }
+
+            return age;
+        }
+    }
+   }
