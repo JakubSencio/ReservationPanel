60edeedfe98ea7a633a9580d87f2f7f21a5a28f1
Jacob <116436495+JakubSencio@users.noreply.github.com>
2024-01-10 13:26:38 +0100

    Delete ASP.NET 3 - 4 directory

--- ASP.NET 3 - 4/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ASP.NET_3___4.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
