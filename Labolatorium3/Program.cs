ec36f873985af52e5dacf1a9209f3628e6442630
Jakub Sencio <jakub.sencio@microsoft.wsei.edu.pl>
2024-01-27 08:58:27 +0100

    Update Program.cs
    .

--- Labolatorium3/Program.cs
+++ Labolatorium3/Program.cs
@@ -17,7 +17,7 @@ namespace Labolatorium3
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                // dThe default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
